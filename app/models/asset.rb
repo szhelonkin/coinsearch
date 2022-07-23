16a3d455a3d81a3ba60c94aee33f29ce04506946
szhelonkin <zsergey@gmail.com>
2022-07-23 22:24:32 +0200

    testing asset

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,2 +1,3 @@
 class Asset < ApplicationRecord
+  validates :name, presence: true
 end
--- test/models/asset_test.rb
+++ test/models/asset_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class AssetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save asset without name" do
+    asset = Asset.new
+    assert_not asset.save, "Saved the asset without name."
+  end
 end
