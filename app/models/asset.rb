3e83155787ab0dd4419c293ff7da7f2dcb9bec67
szhelonkin <zsergey@gmail.com>
2022-07-23 14:21:26 +0200

    initial commit

---
+++ app/models/asset.rb
@@ -0,0 +1,2 @@
+class Asset < ApplicationRecord
+end
---
+++ db/migrate/20220722153950_create_assets.rb
@@ -0,0 +1,9 @@
+class CreateAssets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :assets do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
