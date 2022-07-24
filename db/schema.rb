c436ac888c4baa96386281437fbe12e64e4b4c50
szhelonkin <zsergey@gmail.com>
2022-07-24 15:11:01 +0200

    add tag types

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_153950) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_24_130250) do
   create_table "assets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tag_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
