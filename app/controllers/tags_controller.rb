a01ec3fdbfc4344430291a7a028b6f3af34a7fa7
szhelonkin <zsergey@gmail.com>
2022-07-29 21:12:12 +0200

    dev

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,36 @@ class TagsController < ApplicationController
   def list
     @tag_types = TagType.all
   end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(params.require(:tag).permit(:name))
+
+    if @tag.save
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(params.require(:tag).permit(:name))
+       redirect_to :action => 'list'
+    else
+      render :action => 'edit'
+    end
+  end
+
 end
