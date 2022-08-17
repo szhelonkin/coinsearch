fde91a5e66d05059c4438645c18e4e2b076322ff
szhelonkin <zsergey@gmail.com>
2022-08-17 11:16:26 +0200

    dev

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,8 +1,28 @@
 class CoinsController < ApplicationController
   def index
+    @assets = Asset.all
   end
 
   def new
     @asset = Asset.new
   end
+
+  def show
+    @asset = Asset.find(params[:id])
+  end
+
+  def edit
+    @asset = Asset.find(params[:id])
+  end
+
+  def create
+    @asset = Asset.new(params.require(:asset).permit(:name))
+
+    if @asset.save
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
 end
