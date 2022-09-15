fd7e3f28913e2311f391f2f80820c29702983403
szhelonkin <zsergey@gmail.com>
2022-09-15 16:02:56 +0200

    dev

---
+++ app/controllers/asset_tags_controller.rb
@@ -0,0 +1,15 @@
+class AssetTagsController < ApplicationController
+  def new
+    @asset_tag = AssetTag.new
+  end
+
+  def create
+    @asset_tag = AssetTag.new(params.require(:asset_tag).permit(:name))
+
+    if @asset_tag.save
+      redirect_to :action => 'new'
+    else
+      render :action => 'new'
+    end
+  end
+end
